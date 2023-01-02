4b33b0d7bfc03d16e7df6e274d950acdf2d3bf7b
Jeddi212 <jedediahf51@gmail.com>
2023-01-02 12:07:16 +0700

    🏸 :: from into

--- conversions/from_into.rs
+++ conversions/from_into.rs
@@ -35,10 +35,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let s: Vec<&str> = s.split(',').collect();
+
+        // Comma special cases
+        if s.len() != 2 {
+            return Person::default();
+        }
+
+        if s[0].len() < 1 {
+            return Person::default();
+        }
+
+        match s[1].parse::<usize>() {
+            Ok(age) => Person {
+                name: s[0].to_string(),
+                age,
+            },
+            _ => Person::default(),
+        }
     }
 }
 
